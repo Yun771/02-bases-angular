68a482d96f630eef7b3bfad1045f268f62fd07a9
Ever Martinez <reve_1997@hotmail.com>
2023-05-05 16:01:19 -0600

    inicio forms

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,7 +1,9 @@
-import { NgModule }          from '@angular/core';
-import { CommonModule }      from '@angular/common';
-import { MainPageComponent } from "./pages/main-page.component";
-import { ListComponent } from './components/list/list.component';
+import { NgModule }     from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule }  from "@angular/forms";
+
+import { MainPageComponent }     from "./pages/main-page.component";
+import { ListComponent }         from './components/list/list.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 @NgModule( {
@@ -13,6 +15,7 @@ import { AddCharacterComponent } from './components/add-character/add-character.
     exports: [MainPageComponent],
     imports: [
         CommonModule,
+        FormsModule,
     ],
 } )
 export class DbzModule {
